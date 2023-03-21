c58b2f8fc54164bd97ca867af95fc3854ada5839
EckCiting <eckciting@outlook.com>
2023-03-21 13:42:27 +0000

    Feat: implement search function

---
+++ src/pages/Search.vue
@@ -0,0 +1,27 @@
+<template>
+  <div style="height:100%">
+    <Topbar></Topbar>
+    <v-row class="ma-0" style="height:100%">
+      <v-col cols="2" class="pa-0">
+        <Menu></Menu>
+      </v-col>
+      <v-col cols="10" class="pa-0" style="background-color: #99CCFF ">
+        <SeachCard/>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import Editor from "@/components/CurNoteEditor.vue";
+import Topbar from "@/components/topbar.vue";
+import Menu from "@/components/Menu.vue";
+import SeachCard from "@/components/SeachCard.vue";
+export default {
+  components: {SeachCard, Menu, Topbar, Editor},
+  name: "Search"
+}
+</script>
+
+<style scoped>
+</style>
